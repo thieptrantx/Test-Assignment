621656a5d75cd7aab485112eb719575a768ab072
Thiep Tran <thiep.tran@toradex.com>
2024-03-26 16:34:49 +0700

    init all tests

--- test2.php
+++ test2.php
@@ -2,7 +2,7 @@
 <hr />
 <p>e.g https://wwww.example.com/api/webhook-enpoint</p>
 <ul>
-	<li>1. Write a php function to detect webhook enpoint data (POST method, json response)</li>
+	<li>1. Write a php function to detect webhook endpoint data (POST method, json response)</li>
 	<li>2. Write data to webhook.txt file</li>
 </ul>
 <!-- Write your answer below -->
