03135f988057ce5b575dee377ed8b21c59f5b775
Thiep Tran <thiep.tran@toradex.com>
2024-04-01 09:19:02 +0700

    tanay test

--- home1.php
+++
@@ -1,8 +0,0 @@
-<h1>Webhook API</h1>
-<hr />
-<p>e.g https://wwww.example.com/api/webhook-enpoint</p>
-<ul>
-	<li>1. Write a php function to detect webhook endpoint data (POST method, json response)</li>
-	<li>2. Write data to webhook.txt file</li>
-</ul>
-<!-- Write your answer below -->
